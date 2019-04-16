91c94b99adcbf64eef673c5124e64c2e33d08fd8
KelseyGibbs <gibbs.kelseylee@gmail.com>
2019-04-16 10:10:41 -0500

    fixed bugs

--- liri.js
+++ liri.js
@@ -4,11 +4,11 @@ var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
 var keys = require("./keys.js");
-var request = require("request");
 var queryUrl;
 var spotify = new Spotify(keys.spotify);
 var search = process.argv[2];
 var userInput = process.argv.slice(3).join(" ");
+var break1 = "\n----------------------------------------------\n";
 
 
 if (!search) {
@@ -31,14 +31,20 @@ switch (search) {
 }
 
 function searchBands(userInput) {
+    if (!userInput) {
+        userInput = "Pink";
+    }
     queryUrl = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
     axios
         .get(queryUrl)
         .then(
             function (response) {
                 var eventDateTime = moment(response.data[0].datetime);
-                console.log("Next Concert Veunue: " + response.data[0].venue.name + "\nLocation: " + response.data[0].venue.city +
-                    "\nDate & Time: " + eventDateTime.format("dddd, MMMM Do YYYY"));
+                console.log(break1 
+                    + "Next Concert Veunue: " + response.data[0].venue.name +
+                    "\nLocation: " + response.data[0].venue.city +
+                    "\nDate & Time: " + eventDateTime.format("dddd, MMMM Do YYYY") + 
+                    break1);
             })
         .catch(function (error) {
             console.log(error);
@@ -46,7 +52,7 @@ function searchBands(userInput) {
 }
 
 function searchSongs(userInput) {
-    if (userInput === undefined || null) {
+    if (!userInput) {
         userInput = "The Sign Ace of Base";
     }
     spotify
@@ -60,40 +66,40 @@ function searchSongs(userInput) {
                 } else {
                     for (i = 0; i < data.tracks.items.length && i < 1; i++) {
                         var musicQuery = data.tracks.items[i];
-                        console.log("Artist: " + musicQuery.artists[0].name +
+                        console.log(break1 +
+                            "Artist: " + musicQuery.artists[0].name +
                             "\nSong Name: " + musicQuery.name +
                             "\nLink to Song: " + musicQuery.preview_url +
-                            "\nAlbum Name: " + musicQuery.album.name)
+                            "\nAlbum Name: " + musicQuery.album.name + 
+                            break1)
                     }
                 };
             });
 }
 
-function searchMovies(userInput) {
-    if (userInput === undefined || null) {
-        userInput = "Mr.Nobody";
+function searchMovies(userInput){
+    if (!userInput) {
+        userInput = "Mr. Nobody";
     }
+    axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy")
+        .then(
+            function (response) {
+                console.log(break1 +
+                    "Title: " + response.data.Title +
+                    "\nYear: " + response.data.Year +
+                    "\nIMDB Rating: " + response.data.imdbRating +
+                    "\nRotten Tomatoes Rating: " + response.data.Ratings[1].Value +
+                    "\nCountry: " + response.data.Country +
+                    "\nLanguage: " + response.data.Language +
+                    "\nPlot: " + response.data.Plot +
+                    "\nActors: " + response.data.Actors + break1);
+            }
+        )
+        .catch((err) => {
+            console.log(err);
+        })
 
-
-
-
-    queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&r=json",
-    request(queryUrl, function (error, response, body) {
-        if (!error && response.statusCode === 200) {
-            
-            // for (i = 0; i < movieData.length && i < 1; i++) {
-                // console.log("Movie Title: " + movieData.Title +
-                //     "\nYear: " + movieData.released +
-                //     "\nIMDB Rating: " + movieData.imdbRating +
-                //     "\nRotten Tomatoes Rating: " + movieData.Ratings[1].Value +
-                //     "\nCountry: " + movieData.Country +
-                //     "\nLanguage: " + movieData.Language +
-                //     "\nPlot: " + movieData.Plot +
-                //     "\nActors: " + movieData.Actors)
-            // };
-        };
-    });
-}
+};
 
 function searchStuff(userInput) {
     fs.readFile("random.txt", "utf8", function (err, data) {
